bdea763da4a8276a8f6f370abcd158c478db44cf
NABOO\Thomas <thomasvandenbossche94@gmail.com>
2017-10-28 14:41:00 +0200

    lijst output geschreven

--- src/model/OutStats.java
+++ src/model/OutStats.java
@@ -4,8 +4,11 @@
 package model;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,8 +22,7 @@ import util.Score;
  * @purpose Generate exercise statistics
  *
  */
-public class OutStats
-{
+public class OutStats {
 	private Date dateOfTest;
 	private long duration;
 	private String selections;
@@ -30,8 +32,7 @@ public class OutStats
 	/**
 	 * @return the dateOfTest
 	 */
-	public Date getDateOfTest()
-	{
+	public Date getDateOfTest() {
 		return dateOfTest;
 	}
 
@@ -39,16 +40,14 @@ public class OutStats
 	 * @param dateOfTest
 	 *            the dateOfTest to set
 	 */
-	public void setDateOfTest(Date dateOfTest)
-	{
+	public void setDateOfTest(Date dateOfTest) {
 		this.dateOfTest = dateOfTest;
 	}
 
 	/**
 	 * @return the duration
 	 */
-	public long getDuration()
-	{
+	public long getDuration() {
 		return duration;
 	}
 
@@ -56,16 +55,14 @@ public class OutStats
 	 * @param duration
 	 *            the duration to set
 	 */
-	public void setDuration(long duration)
-	{
+	public void setDuration(long duration) {
 		this.duration = duration;
 	}
 
 	/**
 	 * @return the selections
 	 */
-	public String getSelections()
-	{
+	public String getSelections() {
 		return selections;
 	}
 
@@ -73,16 +70,14 @@ public class OutStats
 	 * @param selections
 	 *            the selections to set
 	 */
-	public void setSelections(String selections)
-	{
+	public void setSelections(String selections) {
 		this.selections = selections;
 	}
 
 	/**
 	 * @return the finalResult
 	 */
-	public String getFinalResult()
-	{
+	public String getFinalResult() {
 		return finalResult;
 	}
 
@@ -90,8 +85,7 @@ public class OutStats
 	 * @param finalResult
 	 *            the finalResult to set
 	 */
-	public void setFinalResult(String finalResult)
-	{
+	public void setFinalResult(String finalResult) {
 		this.finalResult = finalResult;
 	}
 
@@ -101,18 +95,28 @@ public class OutStats
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "Stats:" + separator + separator + "Date and time of test: " + getDateOfTest() + separator
 				+ "Duration of test: " + getDuration() + " seconds" + separator + "Selected properties: " + separator
 				+ getSelections() + separator + "Final result: " + getFinalResult();
 	}
-	
+
 	public void ListOut(File f, ArrayList<Score> e) throws IOException {
 		FileOutputStream fileOutputStream = new FileOutputStream(f);
 		@SuppressWarnings({ "unused", "resource" })
 		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 		objectOutputStream.writeObject(e);
+	}
+
+	public ArrayList<Score> LijstInlezen(File file) throws ClassNotFoundException, IOException {
+		FileInputStream fileInputStream = new FileInputStream(file);
+		@SuppressWarnings("resource")
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+		@SuppressWarnings("unchecked")
+		ArrayList<Score> f = (ArrayList<Score>) objectInputStream.readObject();
+		return f;
+
 	}
 
 }
--- src/util/Score.java
+++ src/util/Score.java
@@ -16,6 +16,11 @@ import java.util.ArrayList;
 public class Score implements Serializable
 {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	ArrayList<Score> ScoreList = new ArrayList<Score>();
 
 	/*
