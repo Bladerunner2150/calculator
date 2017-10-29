52f943a5bb0bb3aa83e01e331fa3e20593fc1147
Bart <bart@taelemans.org>
2017-10-29 17:35:56 +0100

    input validation voor eerste vraag (nog aan te vullen)

--- src/view/TestCalculator.java
+++ src/view/TestCalculator.java
@@ -3,6 +3,7 @@
  */
 package view;
 
+import java.awt.HeadlessException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -30,18 +31,21 @@ import util.Score;
  * @purpose Test calculator
  *
  */
-public class TestCalculator {
+public class TestCalculator
+{
 
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException
+	{
 		Random random = new Random();
 		char input;
 		String userinput = " ";
 		int tafel = 0;
 		boolean tafelOefening = false;
+		boolean success = false;
 
 		int numberCalculations = 0;
 		ArrayList<Character> bewerkingen = new ArrayList<>();
@@ -55,20 +59,38 @@ public class TestCalculator {
 
 		// loop voor nieuwe oefening: alle variabelen leegmaken
 
-		userinput = JOptionPane.showInputDialog(
-				"Wat wil je doen?\n1. Bestaande settings inladen van een file\n2. Nieuwe settings ingeven\n3. Tafels oefenen");
-		switch (userinput) {
+		while (!success)
+		{
+			try
+			{
+				do
+				{
+					userinput = JOptionPane.showInputDialog(
+							"Wat wil je doen?\n1. Bestaande settings inladen van een file\n2. Nieuwe settings ingeven\n3. Tafels oefenen");
+				} while (Integer.parseInt(userinput) < 1 || Integer.parseInt(userinput) > 3);
+				success = true;
+			} catch (Exception e1)
+			{
+				JOptionPane.showMessageDialog(null, "Er is iets misgegaan: " + e1.getMessage());
+			}
+		}
+
+		switch (userinput)
+		{
 		case "1":
 			props = new Properties();
 			properties = new InProperties();
-			try {
+			try
+			{
 				FileInputStream inputProps = new FileInputStream("calculatorDefault.properties");
 				props.load(inputProps);
 				inputProps.close();
 
-			} catch (FileNotFoundException e) {
+			} catch (FileNotFoundException e)
+			{
 				JOptionPane.showMessageDialog(null, "File not found");
-			} catch (IOException e) {
+			} catch (IOException e)
+			{
 				JOptionPane.showMessageDialog(null, e.getMessage());
 			}
 			properties.setRangeOfNumbers(Integer.parseInt(props.getProperty("range")));
@@ -76,7 +98,8 @@ public class TestCalculator {
 			properties.setNaturalNumbers(Boolean.parseBoolean(props.getProperty("natural")));
 			properties.setCijfersNaKomma(Integer.parseInt(props.getProperty("decimals")));
 			int calc = props.getProperty("calc").length();
-			for (int i = 0; i < calc; i++) {
+			for (int i = 0; i < calc; i++)
+			{
 				input = props.getProperty("calc").charAt(i);
 				bewerkingen.add(input);
 			}
@@ -90,21 +113,26 @@ public class TestCalculator {
 			numberCalculations = Integer.parseInt(userinput);
 
 			List<String> rangtelwoorden = Arrays.asList("eerste", "tweede", "derde", "vierde");
-			for (int i = 0; i < numberCalculations; i++) {
+			for (int i = 0; i < numberCalculations; i++)
+			{
 				userinput = JOptionPane.showInputDialog("Geef " + rangtelwoorden.get(i) + " bewerking in: ");
 				input = userinput.charAt(0);
-				if (bewerkingen.isEmpty() == true) {
+				if (bewerkingen.isEmpty() == true)
+				{
 					bewerkingen.add(input);
-				} else {
+				} else
+				{
 
 					// If- statement om te controleren of ze niet twee keer dezelfde bewerking
 					// ingeven
-					if (bewerkingen.contains(input) == true) {
+					if (bewerkingen.contains(input) == true)
+					{
 
 						JOptionPane.showMessageDialog(null, "Geef andere bewerking in ");
 						// eentje aftrekken bij "i" omdat de for-loop anders stopt
 						i--;
-					} else {
+					} else
+					{
 						bewerkingen.add(input);
 					}
 				}
@@ -116,41 +144,50 @@ public class TestCalculator {
 
 			userinput = JOptionPane.showInputDialog("Wil je alleen met positieve getallen rekenen? (j/n)");
 			input = Character.toLowerCase(userinput.charAt(0));
-			if (input == 'j') {
+			if (input == 'j')
+			{
 				properties.setPositiveNumbers(true);
-			} else {
+			} else
+			{
 				properties.setPositiveNumbers(false);
 			}
 
 			userinput = JOptionPane.showInputDialog("Wil je alleen met gehele getallen rekenen? (j/n)");
 			input = Character.toLowerCase(userinput.charAt(0));
-			if (input == 'j') {
+			if (input == 'j')
+			{
 				properties.setNaturalNumbers(true);
-			} else {
+			} else
+			{
 				properties.setNaturalNumbers(false);
 			}
 
-			if (!properties.isNaturalNumbers()) {
+			if (!properties.isNaturalNumbers())
+			{
 				userinput = JOptionPane.showInputDialog("Hoeveel cijfers na de komma?");
 				properties.setCijfersNaKomma(Integer.parseInt(userinput));
 			}
 
-//			if (bewerkingen.contains('*')) // test om te kijken of vermenigvuldigingen in lijst bewerkingen zitten
-//			{
-//				userinput = JOptionPane.showInputDialog("Wil je ook een tafel berekenen? (j/n)");
-//				input = Character.toLowerCase(userinput.charAt(0));
-//				if (input == 'j') {
-//					do {
-//						userinput = JOptionPane
-//								.showInputDialog("Welke tafel wil je berekenen? Geef een cijfer van 1 tot 9");
-//						properties.setMultiplyTable(Integer.parseInt(userinput));
-//					} while (properties.getMultiplyTable() < 1 || properties.getMultiplyTable() > 9);
-//				}
-//			}
+			// if (bewerkingen.contains('*')) // test om te kijken of vermenigvuldigingen in
+			// lijst bewerkingen zitten
+			// {
+			// userinput = JOptionPane.showInputDialog("Wil je ook een tafel berekenen?
+			// (j/n)");
+			// input = Character.toLowerCase(userinput.charAt(0));
+			// if (input == 'j') {
+			// do {
+			// userinput = JOptionPane
+			// .showInputDialog("Welke tafel wil je berekenen? Geef een cijfer van 1 tot
+			// 9");
+			// properties.setMultiplyTable(Integer.parseInt(userinput));
+			// } while (properties.getMultiplyTable() < 1 || properties.getMultiplyTable() >
+			// 9);
+			// }
+			// }
 
 			userinput = JOptionPane.showInputDialog("Hoeveel oefeningen wil je maken?");
 			properties.setNumberOfExercises(Integer.parseInt(userinput));
-			
+
 			// wegschrijven naar properties file
 			props = new Properties();
 			props.setProperty("range", String.valueOf(properties.getRangeOfNumbers()));
@@ -170,12 +207,12 @@ public class TestCalculator {
 				FileOutputStream output = new FileOutputStream("calculatorDefault.properties");
 				props.store(output, "Opgeslagen selecties");
 				output.close();
-				
+
 			} catch (IOException e)
 			{
 				JOptionPane.showMessageDialog(null, e.getMessage());
 			}
-			
+
 			break;
 		case "3":
 			tafelOefening = true;
@@ -194,8 +231,10 @@ public class TestCalculator {
 		Date currentDate = new Date();
 		stats.setDateOfTest(currentDate);
 
-		if (tafelOefening) {
-			for (int i = 1; i <= 10; i++) {
+		if (tafelOefening)
+		{
+			for (int i = 1; i <= 10; i++)
+			{
 				double oplossing = calculator.result(i, tafel, '*');
 				score.setOplossing(oplossing);
 				userinput = JOptionPane.showInputDialog("Hoeveel is " + i + "*" + tafel + " ?");
@@ -207,8 +246,10 @@ public class TestCalculator {
 			stats.setSelections("Tafel van " + tafel);
 		}
 
-		else {
-			for (int i = 0; i < properties.getNumberOfExercises(); i++) {
+		else
+		{
+			for (int i = 0; i < properties.getNumberOfExercises(); i++)
+			{
 				int index = random.nextInt(properties.getCalculations().size());
 				char calculation = properties.getCalculations().get(index);
 				double randomgetal1 = generator.RandomGetal(properties);
@@ -216,7 +257,7 @@ public class TestCalculator {
 				double result = calculator.result(randomgetal1, randomgetal2, calculation);
 				score.setOplossing(result);
 				userinput = JOptionPane.showInputDialog("Oefening " + (i + 1) + "/" + properties.getNumberOfExercises()
-						+ ": Hoeveel is " + randomgetal1 + calculation + randomgetal2 + " ?");
+						+ ": Hoeveel is " + randomgetal1 + " " + calculation + " " + randomgetal2 + " ?");
 				double answer = Double.parseDouble(userinput);
 				score.setUserGetal(answer);
 
@@ -232,7 +273,8 @@ public class TestCalculator {
 		stats.setFinalResult(score.getAantalJuisteAntwoorden() + "/" + score.getScoreList().size());
 		File f = new File("test.txt");
 		OutStats.ListOut(f, score.getScoreList());
-		//OutStats.ListOut(f, stats.toString()); // eventueel input parameter van ListOut op String zetten
+		// OutStats.ListOut(f, stats.toString()); // eventueel input parameter van
+		// ListOut op String zetten
 		JOptionPane.showMessageDialog(null, score.toString());
 		JOptionPane.showMessageDialog(null, stats.toString());
 	}
