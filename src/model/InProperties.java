3c0b29ba21659c53767f28f148c28155228648dc
Bart <bart@taelemans.org>
2017-10-30 20:17:38 +0100

    remove unused

--- src/model/InProperties.java
+++ src/model/InProperties.java
@@ -3,11 +3,8 @@
  */
 package model;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
--- src/view/TestCalculator.java
+++ src/view/TestCalculator.java
@@ -6,7 +6,6 @@ package view;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
